e0c14fa0071e6ff422295d55d6f6c0e03a5ae131
Marilin Adams <marilin@limonaad.org>
2012-09-19 03:32:06 +0100

    Stupid way of disabling contact page title but couldn't find any better way.

--- sites/all/themes/tb_rave/template.php
+++ sites/all/themes/tb_rave/template.php
@@ -5,3 +5,6 @@
  */
 
 require_once drupal_get_path('theme', 'tb_rave') . '/inc/preprocess_functions.inc';
+function tb_rave_form_contact_site_form_alter(&$form, &$form_state) {
+  drupal_set_title('');
+}
